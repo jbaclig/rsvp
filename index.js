9e41cbd17679536c022089ab1da6b906129b7c44
Jon Baclig <jon@baclig.com>
2018-08-17 10:34:25 -0700

    updated routes and index.js

--- index.js
+++ index.js
@@ -4,23 +4,6 @@ var routes = require("./routes/routes.js");
 const path = require('path')
 var app = express();
 const PORT = process.env.PORT || 5000;
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
-
-app.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect()
-    const result = await client.query('SELECT * FROM test_table');
-    res.render('pages/db', result);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,9 +12,7 @@ routes(app);
 
 var server = app
     .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
+    
     .listen(PORT, function () {
         console.log(`Listening on ${PORT}`);
     });
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,27 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+
 var appRouter = function (app) {
     app.get("/", function(req, res) {
-      res.status(200).send("Welcome to our restful API");
+      res.status(200).send("Welcome to our restful API!!!");
     });
-  }
+
+    app.get('/db', async (req, res) => {
+        console.log('db');
+        try {
+          const client = await pool.connect()
+          const result = await client.query('SELECT * FROM test_table');
+          res.render('pages/db', result);
+          client.release();
+        } catch (err) {
+          console.error(err);
+          res.send("Error " + err);
+        }
+      });
+}
   
-  module.exports = appRouter;   
+module.exports = appRouter;   
