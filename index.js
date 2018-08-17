38d90a3e1e2b3aab9b47fca6b927d737ba0c3fc6
Jon Baclig <jon@baclig.com>
2018-08-17 09:55:41 -0700

    add postgress db

--- index.js
+++ index.js
@@ -3,7 +3,24 @@ var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
 const path = require('path')
 var app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    res.render('pages/db', result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
